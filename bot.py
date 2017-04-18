6be91dd951ae7b3650279482359189d7dd8d6a7e
Akeyro <neoshadow4300@gmail.com>
2017-04-18 22:41:49 +0200

    Test for the mention

--- bot.py
+++ bot.py
@@ -138,7 +138,7 @@ async def startsch(ctx):
 #Scheduler --------------------------------
 async def schedule():
     await client.wait_until_login()
-    twtr = 19
+    twtr = 20
     st1 = 22
     st2 = 3
     twtr_trigger = 1
